7d93f0b2ca7fcc011fc78fe3b1385165ae807a98
Satvika Reddy Kasireddy <skasireddy@worldbankgroup.org>
2022-03-31 21:03:58 +0530

    update

--- scripts.js
+++ scripts.js
@@ -611,17 +611,17 @@ async function loadEager(doc) {
   var target = document.querySelectorAll('.logo-wrapper')[0];
   target.after(parent2);
 
-  var columns = document.querySelectorAll('.columns');
-  var column2 = document.querySelectorAll('.column-2');
-  columns.forEach((column) => {
+  var columns = document.querySelectorAll('.columns')[0];
+  var column2 = document.querySelectorAll('.column-2')[0];
+  // columns.forEach((column) => {
     var wrapper = document.createElement('div');
     wrapper.className = "full-row-white-components";
     var container = document.createElement('div');
     container.className = "container";
-    wrapper.appendChild(container).append(column, column2);
-    var target = document.querySelectorAll('.primary-subnav-items')[0];
-    target.after(wrapper);
-  })
+    wrapper.appendChild(container).append(columns, column2);
+    var datatarget = document.querySelectorAll('.primary-subnav-items')[0];
+    datatarget.after(wrapper);
+  // })
 
   // const theme = getMetadata('theme');
   // if (theme) document.body.classList.add(toClassName(theme));
